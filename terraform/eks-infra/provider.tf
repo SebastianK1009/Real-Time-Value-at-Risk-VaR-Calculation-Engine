461f50b21e4dc9857c29085b1ee88b2ad248cf7d
Sebastian <sebastianic1009@gmail.com>
2026-01-19 16:45:35 +0000

    add Makefile for EKS infrastructure deployment and management

--- terraform/eks-infra/provider.tf
+++ terraform/eks-infra/provider.tf
@@ -26,15 +26,23 @@ provider "aws" {
 # These two will connect Terraform to your EKS cluster after it's created
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data) # cluster_certificate_authority_data is from eks module output
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+    command     = "aws"
+  }
 }
 
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+      command     = "aws"
+    }
   }
 }
 
